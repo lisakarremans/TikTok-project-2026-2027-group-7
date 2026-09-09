b81f76dc109af6d624f5ead849288aeadb7645ad
Lisa Karremans <l.karremans@tilburguniversity.edu>
2026-09-09 14:59:34 +0200

    Finish rscript to clean the data

--- src/Deliverable_2/clean_data.r
+++ src/Deliverable_2/clean_data.r
@@ -1,6 +1,7 @@
 # Clean the dataset
 library(dplyr)
 library(tidyverse)
-users <- read.csv("data/raw/users.csv")
+users <- read.csv("../../data/raw/users.csv")
 users_clean <- users %>%
   select(-user_name, -user_handle)
+write.csv(users_clean, "../../data/raw/users_clean.csv", row.names = FALSE)
