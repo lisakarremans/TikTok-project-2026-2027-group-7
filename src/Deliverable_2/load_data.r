20637aa37030742901e5ed0cca9cd610cdbd313a
Lisa Karremans <l.karremans@tilburguniversity.edu>
2026-09-09 15:08:17 +0200

    Update rscript to load the data

--- src/Deliverable_2/load_data.r
+++ src/Deliverable_2/load_data.r
@@ -1,4 +1,4 @@
 # Download the data
 data_url <- paste0("https://raw.githubusercontent.com/hannesdatta/course-dprep/refs/heads/main/material/project/coaching_2_data/users.csv")
-download.file(data_url, "data/raw/users.csv", mode = "wb")
+download.file(data_url, "../../data/raw/users.csv", mode = "wb")
 list.files()
